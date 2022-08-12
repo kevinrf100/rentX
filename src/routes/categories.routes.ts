ac3eaf0dee68a50e585c1d0c0d2ed76d431abb39
Kevin Rossetti Fernandes <kevinrf100@gmail.com>
2022-08-11 21:15:48 -0300

    Adding uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,23 @@
 import { Request, Response, Router }  from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request: Request, response: Response) => {
+categoriesRoutes.post("/", (request: Request, response: Response) => {
     const { name, description } = request.body;
 
-    categories.push({
+    const category = {
+        id: uuidV4(),
         name,
-        description
-    });
+        description,
+        created_at: new Date()
+    };
 
-    return response.status(201).send();
+    categories.push(category);
+
+    return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response, response }  from "express";
+import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
     console.log("Server is Running");
